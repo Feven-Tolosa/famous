2e6716c974b208045d9ac8c1d9c8947270439dff
Feven-Tolosa <tolosafeven0@gmail.com>
2025-05-09 23:35:38 +0300

    updats on db

--- app/models/Analytics.js
+++ app/models/Analytics.js
@@ -1,14 +1,10 @@
-// models/Analytics.js
 import mongoose from 'mongoose'
 
-const analyticsSchema = new mongoose.Schema({
+const schema = new mongoose.Schema({
   influencerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Influencer' },
   date: { type: Date, default: Date.now },
   followers: Number,
-  engagementRate: Number,
-  platform: String,
 })
 
-// Prevent model overwrite in dev mode
-export default mongoose.models?.Analytics ||
-  mongoose.model('Analytics', analyticsSchema)
+// Avoid OverwriteModelError
+export default mongoose.models?.Analytics || mongoose.model('Analytics', schema)
